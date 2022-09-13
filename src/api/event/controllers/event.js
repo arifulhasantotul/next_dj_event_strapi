b618a67c8b52c6e186eac55468ae2bfb0503472b
arifulhasantotul <mdahtotul@gmail.com>
2022-09-13 15:59:12 +0600

    slug and id both working to get single event

--- src/api/event/controllers/event.js
+++ src/api/event/controllers/event.js
@@ -6,20 +6,19 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-// module.exports = createCoreController("api::event.event");
+module.exports = createCoreController("api::event.event");
 
 // REMEMBER: if u want you url like http://localhost:1337/api/event/${slug} instead of http://localhost:1337/api/event/${id} you need use below code
 
-module.exports = createCoreController("api::event.event", ({ strapi }) => ({
-  async findOne(ctx) {
-    const { id } = ctx.params;
-    console.log(ctx.params);
+// module.exports = createCoreController("api::event.event", ({ strapi }) => ({
+//   async findOne(ctx) {
+//     const { id } = ctx.params;
 
-    const entity = await strapi.db
-      .query("api::event.event")
-      .findOne({ where: { slug: id }, populate: true }); // matching slug field in database
-    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+//     const entity = await strapi.db
+//       .query("api::event.event")
+//       .findOne({ where: { slug: id }, populate: true }); // matching slug field in database
+//     const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
 
-    return this.transformResponse(sanitizedEntity);
-  },
-}));
+//     return this.transformResponse(sanitizedEntity);
+//   },
+// }));
